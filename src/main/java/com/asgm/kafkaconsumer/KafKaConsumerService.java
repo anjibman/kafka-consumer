b0264450769149462d336de2c725d7aad87b8ad9
anjibman <anjibcs@hotmail.com>
2022-05-21 12:57:20 -0500

    Consumer handling JSON object

--- src/main/java/com/asgm/kafkaconsumer/KafKaConsumerService.java
+++ src/main/java/com/asgm/kafkaconsumer/KafKaConsumerService.java
@@ -1,5 +1,6 @@
 package com.asgm.kafkaconsumer;
 
+import com.asgm.model.User;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.kafka.annotation.KafkaListener;
@@ -14,5 +15,11 @@ public class KafKaConsumerService {
     public void consume(String message)
     {
         logger.info(String.format("Message received -> %s", message));
+    }
+
+    @KafkaListener(topics = "${kafka.topic_name}", groupId = "${kafka.group_id}")
+    public void consume(User user)
+    {
+        logger.info(String.format("Message received -> %s", user.toString()));
     }
 }
